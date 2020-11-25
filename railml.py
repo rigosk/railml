07cd6f8ddaf7cd8a8bce941a0da9937b75f46a17
rigosk001 <rigosk001@gmail.com>
2020-11-25 17:01:50 +0200

    Update railml.py

--- railml.py
+++ railml.py
@@ -5,7 +5,7 @@ import gc
 import base64
 
 
-st.set_page_config(     
+st.set_page_config(
      page_title="railML file ",
      page_icon="🛤️",
      layout="wide",
@@ -18,7 +18,32 @@ footer {visibility: hidden;}
 </style>
 
 """
-st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
+
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index = False, sheet_name='Sheet1')
+    workbook  = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) # Tried with '0%' and '#,##0.00' also.
+    worksheet.set_column('A:A', None, format1) # Say Data are in column A
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
+
+def get_table_download_link(df):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+    val = to_excel(df)
+    b64 = base64.b64encode(val)  # val looks like b'...'
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="Your_File.xlsx">Download Excel file</a>' # decode b'abc' => abc
+
+st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 ns='{http://www.railml.org/schemas/2013}'
 namespaces={'ns':'http://www.railml.org/schemas/2013'}
 
@@ -56,12 +81,12 @@ uploaded_file = st.sidebar.file_uploader("Choose a railML file", type=["railml"]
 # Space out the maps so the first one is 2x the size of the other three
 c1, c2 = st.beta_columns((1, 4))
 if uploaded_file is not None:
-    
+
     df=get_trainParts(uploaded_file)
     uploaded_file.seek(0)
     gc.collect()
     c2.write(df)
-    
+
 
     c2.markdown(get_table_download_link(df), unsafe_allow_html=True)
 placeholder = c1.empty()
@@ -76,4 +101,3 @@ else:
 if not uploaded_file:
     st.warning('Please upload a railml file.')
     st.stop()
-
