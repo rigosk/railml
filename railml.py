9fd0bc44e5d9d079559b6d3341bb2b09e044f46d
rigosk001 <rigosk001@gmail.com>
2021-09-06 16:43:04 +0300

    Update railml.py

--- railml.py
+++ railml.py
@@ -372,7 +372,7 @@ if file_path is not None:
     tree = ET.parse(file_path)
     root = tree.getroot()
     st.write(list(root))
-    st.write(root.findall(ns+".//tracks"))
+    st.write(root.findall(ns+".//tracks").tag)
 
 #    st.write(gc.get_stats())
 #    st.write(gc.get_objects())
